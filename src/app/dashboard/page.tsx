090eef031e64957c9bfbb61e71d4aa4cbba015e0
unknown <samprityghosh234@gmail.com>
2024-11-07 20:21:03 +0530

    dashboard initialization

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+function page() {
+  return (
+    <div className="flex flex-col min-h-screen">
+       This is a page
+    </div>
+  )
+}
+
+export default page
